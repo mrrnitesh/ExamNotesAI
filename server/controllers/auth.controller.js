4feb19ca6c4b35a67f178d83855a49ca468a5c6c
Nitesh Kumar <147733127+mrrnitesh@users.noreply.github.com>
2026-05-01 22:37:07 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -16,7 +16,8 @@ export const googleAuth = async (req, res) => {
             // secure: false,
             // samesite: "strict", 
             secure: true,
-            samesite: "none",
+            sameSite: "none",
+            path: "/",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(200).json(user)
