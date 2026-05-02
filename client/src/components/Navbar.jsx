16eeef726c23c59fc9aa387c190c97d0cb4e17b4
Nitesh Kumar <147733127+mrrnitesh@users.noreply.github.com>
2026-05-02 14:10:40 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,7 +14,8 @@ const Navbar = () => {
     const navigate = useNavigate()
     const dispatch = useDispatch()
     // This line add from chatgpt
-    const serverUrl = "http://localhost:5173/auth";
+   // const serverUrl = "http://localhost:5173/auth";
+    const serverUrl = "https://examnotesaiserver-u6sq.onrender.com/auth";
     const handleSignOut = async () =>{
         try{
             await axios.get(serverUrl + "/api/auth/logout", {withCredentials:true})
