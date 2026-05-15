b688e4eb47581cc2bf97e011c3cd8b20964d877b
Nitesh Kumar <147733127+mrrnitesh@users.noreply.github.com>
2026-05-15 15:36:10 +0530

    Update auth.route.js

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,12 +1,9 @@
 import express from "express"
 import { googleAuth, logOut } from "../controllers/auth.controller.js"
-// Extra
-import { verifyToken } from "../middleware/auth.middleware.js"
 const authRouter = express.Router()
 
 authRouter.post("/google",googleAuth)
 authRouter.get("/louout",logOut)
-// Extra
-authRouter.get("/logout", verifyToken, logout)
+
 
 export default authRouter
