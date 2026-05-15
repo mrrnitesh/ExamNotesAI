e306175cf71ae0dbf458b376ea4416dd07bcb8b0
Nitesh Kumar <147733127+mrrnitesh@users.noreply.github.com>
2026-05-15 12:20:37 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.post(
 );
 
 app.use(cors(
-   { origin:"http://localhost:5173",
+   { origin:"https://examnotesaiclient-l4rx.onrender.com",
   //  { origin:"https://examnotesaiclient-9kj0.onrender.com",
      credentials:true,
      methods:["GET", "POST" , "PUT" , "DELETE" , "OPTIONS"]
